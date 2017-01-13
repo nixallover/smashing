9a62a22548d7fd5aa0f590a08182b4b4086c5f83
Nicole Oliver <Nicole Oliver>
2017-01-12 17:53:29 -0800

    started listing some ingredients

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { INGREDIENTS } from './data/ingredients';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Smush!';
-}
+  public title = 'Smush!';
+  private ingredients: any[] = INGREDIENTS;
+ }
---
+++ src/app/data/ingredients.ts
@@ -0,0 +1,17 @@
+export var INGREDIENTS: any[] = [
+    {
+        id: 1,
+        name: 'Apple',
+        category: 'fruit'
+    },
+    {
+        id: 2,
+        name: 'Banana',
+        category: 'fruit'
+    },
+    {
+        id: 3,
+        name: 'Yogurt',
+        category: 'dairy'
+    }
+];
